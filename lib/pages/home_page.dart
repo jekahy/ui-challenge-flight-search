cdd3cca6a5b66fda9599103458b5c2a20b676de8
Eugene Hyrol <eugene.hyrol@accenture.com>
2019-11-30 11:49:57 +0100

    initial commit - working home page with card

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../components/air_asia_bar.dart';
+import '../components/content_card.dart';
+import '../components/rounded_button.dart';
+
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          AirAsiaBar(height: 210.0),
+          Positioned.fill(
+            child: Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).padding.top + 40.0),
+              child: new Column(
+                children: <Widget>[
+                  _buildButtonsRow(),
+                  Expanded(child: ContentCard()), //TODO: Implement a card
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButtonsRow() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: <Widget>[
+          new RoundedButton(text: "ONE WAY"),
+          new RoundedButton(text: "ROUND"),
+          new RoundedButton(text: "MULTICITY", selected: true),
+        ],
+      ),
+    );
+  }
+}
